d0571c56814409d687e128557dc97b6584793d16
Hodakai <gravy.thom@gmail.com>
2021-04-07 11:42:16 +0200

    Tests réalisés et ok !

--- GRAVY Thomas TDN2.cpp
+++ GRAVY Thomas TDN2.cpp
@@ -31,6 +31,18 @@ void Humain::afficher()
     cout << "Bonjour je m'apelle " << this->nom  << " et je suis encore un Humain" << endl; //Affichage de l'humain pour attester de son humanité
 }
 
+//////////////////////////////////////////////////////////////////////         Fonction CalculNomZombie         ///////////////////////////////////////////////////////////////////
+
+string CalculNomZombie(string Humain)
+{
+    char ch;
+    ch = Humain[0] + 32; //On ajoute 32 au caractère ASCII pour le passer en lowercase
+    Humain = Humain[0]; //Ici j'ai pris le premier caractère du nom de l'humain en première lettre du nom de Zombie
+    Humain += ch;
+    Humain += ch; //J'ai fait comme ça parce que sinon ça multipliait la valeur ASCII de mon caractère par 2 au lieu de l'inscrire 2 fois
+    return Humain; //Return de l'humain transformé
+}
+
 //////////////////////////////////////////////////////////////////////////         Classe Zombie         //////////////////////////////////////////////////////////////////////////
 
 class Zombie
@@ -57,34 +69,25 @@ string Zombie::GetNomZombie() //Methode du getter pour le zombie
 
 Zombie* Zombie::manger(Humain* humain) 
 {
-    Zombie* zombie = new Zombie(humain);
-    cout << "Oh non !!! Notre humain " << humain->GetNomHumain() << " s'est transforme en " << zombie->nom << endl;
-    free(humain);
+    Zombie* zombie = new Zombie(humain); //Création du nouveau zombie
+    cout << "Oh non !!! Notre humain " << humain->GetNomHumain() << " s'est transforme en " << zombie->nom << endl; //Sortie console pour attester de la transformation de notre humain
+    free(humain); //Liberation de la memoire alouée à notre humain qui est désormais un zombie
     return zombie;
 }
 
 void Zombie::afficher() 
 {
-    cout << this->nom << " est un Zombie !!! Courrez !" << endl; //Affichage du zombie pour attester de son non-humanité
+    cout << this->nom << " est un Zombie !!!" << endl; //Affichage du zombie pour attester de son non-humanité
 }
 
 //////////////////////////////////////////////////////////////////////////         Fonctions et Main         //////////////////////////////////////////////////////////////////////////
 
-string CalculNomZombie (string Humain) 
-{
-    char ch;
-    ch = Humain[0] + 32; //On ajoute 32 au caractère ASCII pour le passer en lowercase
-    Humain = Humain[0]; //Ici j'ai pris le premier caractère du nom de l'humain en première lettre du nom de Zombie
-    Humain += ch;
-    Humain += ch; //J'ai fait comme ça parce que sinon ça multipliait la valeur ASCII de mon caractère par 2 au lieu de l'inscrire 2 fois
-    return Humain; //Return de l'humain transformé
-}
-
 int main()
 {
     Humain* humain1 = new Humain("Maurice"); //Nouvelle instance d'un humain avec un pointeur (nom = "Maurice")
+    Zombie* zombie1 = new Zombie(humain1);
     Humain* humain2 = new Humain("Roberto"); //Nouvelle instance d'un humain avec un pointeur (nom = "Roberto")
-    humain1->afficher(); //Affichage de l'humain1 (Maurice)
     humain2->afficher(); //Affichage de l'humain2 (Roberto)
-    cout << "Oh non !!! Notre humain " << humain1->GetNomHumain() << " s'est transforme en " << CalculNomZombie(humain1->GetNomHumain()) << endl;
+    Zombie* zombie2 = zombie1->manger(humain2); //Attribution du nouveau zombie créé à un nouveau zombie
+    zombie2->afficher();
 }
