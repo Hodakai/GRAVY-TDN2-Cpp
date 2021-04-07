7732ae3d2b8f7be76c8ccc10dc9333e90a5cb9fc
Hodakai <gravy.thom@gmail.com>
2021-04-07 10:16:44 +0200

    Ajout de la méthode d'affichage des humains ainsi qu'une optimisation de la methode CalculNomZombie

--- GRAVY Thomas TDN2.cpp
+++ GRAVY Thomas TDN2.cpp
@@ -8,6 +8,7 @@ class Humain
 public:
     string GetNom(); //Le getter
 	Humain(string nom); //Le constructeur
+    void afficher();
     
 private:
     string nom;
@@ -23,22 +24,26 @@ Humain::Humain(string nom)
     this->nom = nom; //On attribue ici la valeur du parametre "nom" dans le champ nom de la classe Humain
 }
 
+void Humain::afficher()
+{
+    cout << "Bonjour je m'apelle " << this->nom  << " et je suis encore un Humain" << endl;
+}
+
 
 string CalculNomZombie (string Humain) {
     char ch;
     ch = Humain[0] + 32; //On ajoute 32 au caractère ASCII pour le passer en lowercase
-    string Zombie;
-    Zombie = Humain[0]; //Ici j'ai pris le premier caractère du nom de l'humain en première lettre du nom de Zombie
-    Zombie += ch;
-    Zombie += ch; //J'ai fait comme ça parce que sinon ça multipliait la valeur ASCII de mon caractère par 2 au lieu de l'inscrire 2 fois
-    return Zombie; //Return de l'humain transformé
+    Humain = Humain[0]; //Ici j'ai pris le premier caractère du nom de l'humain en première lettre du nom de Zombie
+    Humain += ch;
+    Humain += ch; //J'ai fait comme ça parce que sinon ça multipliait la valeur ASCII de mon caractère par 2 au lieu de l'inscrire 2 fois
+    return Humain; //Return de l'humain transformé
 }
 
 int main()
 {
-    string Humain;
-    Humain = "Michel";
-    string Zombie;
-    Zombie = CalculNomZombie(Humain);
-    cout << "Ah non !!! " << Humain << " s'est transforme en " << Zombie << endl;
+    Humain* humain1 = new Humain("Maurice"); //Nouvelle instance d'un humain avec un pointeur (nom = "Maurice")
+    Humain* humain2 = new Humain("Roberto"); //Nouvelle instance d'un humain avec un pointeur (nom = "Roberto")
+    humain1->afficher(); //Affichage de l'humain1 (Maurice)
+    humain2->afficher(); //Affichage de l'humain2 (Roberto)
+    cout << "Oh non !!! Notre humain " << humain1->GetNom() << " s'est transforme en " << CalculNomZombie(humain1->GetNom()) << endl;
 }
